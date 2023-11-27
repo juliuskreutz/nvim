82d3d5e8a58358bd58d2ed88f92515541ea7a416
Julius Kreutz <julius@kreutz.dev>
2023-11-27 20:56:00 +0100

    saving

---
+++ lua/plugins/crates.lua
@@ -0,0 +1,16 @@
+return {
+    "saecki/crates.nvim",
+    tag = "v0.4.0",
+    event = { "BufRead Cargo.toml" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        local crates = require('crates')
+
+        crates.setup()
+
+        vim.keymap.set("n", "<leader>cu", crates.update_all_crates)
+    end,
+}
